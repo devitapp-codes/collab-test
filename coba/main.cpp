c47008eacf72684af936de569f2039105d780cb8
tito bagus <titobagus14@gmail.com>
2016-12-17 15:05:53 +0700

    Added folder coba and modified cpp/hello.cpp

---
+++ coba/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout<<" halo semua coba"<<endl;
+
+
+
+    return 0;
+}
--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -3,4 +3,5 @@ using namespace std;
 
 int main() {
   cout << "Hello world!";
+  cout<<" haloo semua coba";
 }
